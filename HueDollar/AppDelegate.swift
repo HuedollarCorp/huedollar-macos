6d21cf24159ff1b9534ff1a6e7ce915f5bb82767
Alan Sikora <alansikora@gmail.com>
2017-04-17 23:30:37 -0300

    Initial Commit

---
+++ HueDollar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HueDollar
+//
+//  Created by Alan Sikora on 17/04/17.
+//  Copyright © 2017 br.com.hue. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HueDollar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HueDollar
+//
+//  Created by Alan Sikora on 17/04/17.
+//  Copyright © 2017 br.com.hue. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
