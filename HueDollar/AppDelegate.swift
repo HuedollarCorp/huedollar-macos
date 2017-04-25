4ff327377f83bb796a0031e48aad192a213ffd3c
Alan Sikora <alansikora@gmail.com>
2017-04-25 10:59:42 -0300

    initial commit

--- HueDollar/AppDelegate.swift
+++ HueDollar/AppDelegate.swift
@@ -11,10 +11,90 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    var currencyAPI: CurrencyAPI!
+    let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+    var dateMenuItem: NSMenuItem!
+    var payoneerCurrencyItem: NSMenuItem!
+    
+    func getLastQuoteString() -> String {
+        if(UserDefaults.standard.value(forKey: "last_quote") == nil) {
+            UserDefaults.standard.setValue(0.0, forKey: "last_quote")
+        }
+        
+        return String(format: "$ %.4f", UserDefaults.standard.value(forKey: "last_quote") as! Float)
+    }
+    
+    func getLastQuoteDateString() -> String {
+        if(UserDefaults.standard.value(forKey: "last_quote_date") == nil) {
+            UserDefaults.standard.setValue("---", forKey: "last_quote_date")
+        }
+        
+        return "Last Quote: \(UserDefaults.standard.value(forKey: "last_quote_date")!)"
+    }
+    
+    func getLastPayonnerQuoteString() -> String {
+        if(UserDefaults.standard.value(forKey: "last_payoneer_quote") == nil) {
+            UserDefaults.standard.setValue(0.0, forKey: "last_payoneer_quote")
+        }
+        
+        return String(format: "Payoneer: $ %.4f", UserDefaults.standard.value(forKey: "last_payoneer_quote") as! Float)
+    }
+    
+    func getRate() {
+        currencyAPI.fetchCurrency("USD") { currency in
+            UserDefaults.standard.setValue(currency.quote, forKey: "last_quote")
+            UserDefaults.standard.setValue(currency.quote * 0.98, forKey: "last_payoneer_quote")
+            UserDefaults.standard.setValue(currency.when, forKey: "last_quote_date")
+            
+            self.statusItem.menu?.item(withTag: 0)?.title = self.getLastQuoteDateString()
+            self.statusItem.menu?.item(withTag: 1)?.title = self.getLastPayonnerQuoteString()
+            
+            if let button = self.statusItem.button {
+                button.title = self.getLastQuoteString()
+            }
+        }
+    }
+    
+    func getRateEvent(sender: AnyObject) {
+        getRate()
+    }
+    
+    func quit(sender: NSMenuItem) {
+        NSApp.terminate(self)
+    }
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        Timer.scheduledTimer(timeInterval: 1800, target: self, selector: #selector(self.getRate), userInfo: nil, repeats: true);
+        
+        currencyAPI = CurrencyAPI()
+        
+        if let button = statusItem.button {
+            button.title = getLastQuoteString()
+        }
+        
+        let menu = NSMenu()
+        
+        menu.autoenablesItems = false
+        
+        payoneerCurrencyItem = NSMenuItem(title: getLastPayonnerQuoteString(), action: nil, keyEquivalent: "")
+        payoneerCurrencyItem.tag = 1
+        payoneerCurrencyItem.isEnabled = false
+        
+        menu.addItem(payoneerCurrencyItem)
+        
+        dateMenuItem = NSMenuItem(title: getLastQuoteString(), action: nil, keyEquivalent: "")
+        dateMenuItem.tag = 0
+        dateMenuItem.isEnabled = false
+        
+        menu.addItem(dateMenuItem)
+        
+        menu.addItem(NSMenuItem(title: "Get Rate", action: #selector(AppDelegate.getRateEvent(sender:)), keyEquivalent: "R"))
+        menu.addItem(NSMenuItem.separator())
+        menu.addItem(NSMenuItem(title: "Quit", action: #selector(AppDelegate.quit(sender:)), keyEquivalent: "q"))
+        
+        statusItem.menu = menu
+        
+        self.getRate()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
---
+++ HueDollar/CurrencyAPI.swift
@@ -0,0 +1,81 @@
+//
+//  CurrencyLayer.swift
+//  HueDollar
+//
+//  Created by Alan Sikora on 17/04/17.
+//  Copyright © 2017 br.com.hue. All rights reserved.
+//
+
+import Foundation
+
+
+struct Currency : CustomStringConvertible {
+    var currency: String
+    var quote: Float
+    var when: String
+    
+    var description: String {
+        return String(format: "$ %.3f", quote)
+    }
+}
+
+class CurrencyAPI {
+    let BASE_URL = "http://api.promasters.net.br/cotacao/v1/valores"
+    
+    func fetchCurrency(_ currencyCode: String, success: @escaping (Currency) -> Void) {
+        let session = URLSession.shared
+        let url = URL(string: "\(BASE_URL)?alt=json&moedas=\(currencyCode)")
+        let task = session.dataTask(with: url!) { data, response, err in
+            // first check for a hard error
+            if let error = err {
+                NSLog("Currency API error: \(error)")
+            }
+            
+            // then check the response code
+            if let httpResponse = response as? HTTPURLResponse {
+                switch httpResponse.statusCode {
+                case 200: // all good!
+                    if let currency = self.currencyFromJSONData(data!, currencyCode) {
+                        success(currency)
+                    }
+                case 401: // unauthorized
+                    NSLog("Currency API returned an 'unauthorized' response. Did you set your API key?")
+                default:
+                    NSLog("Currency API returned response: %d %@", httpResponse.statusCode, HTTPURLResponse.localizedString(forStatusCode: httpResponse.statusCode))
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func currencyFromJSONData(_ data: Data,_ currencyCode: String) -> Currency? {
+        typealias JSONDict = [String:AnyObject]
+        let json : JSONDict
+        
+        do {
+            json = try JSONSerialization.jsonObject(with: data, options: []) as! JSONDict
+        } catch {
+            NSLog("JSON parsing failed: \(error)")
+            return nil
+        }
+        
+        var valuesDict = json["valores"] as! JSONDict
+        var currencyDict = valuesDict[currencyCode] as! JSONDict
+        
+        let timestamp = NSDate(timeIntervalSince1970: currencyDict["ultima_consulta"] as! Double)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "pt_BR")
+        dateFormatter.timeStyle = DateFormatter.Style.short
+        dateFormatter.dateStyle = DateFormatter.Style.short
+        let localDate = dateFormatter.string(from: timestamp as Date)
+        
+        let currency = Currency(
+            currency: currencyCode,
+            quote: currencyDict["valor"] as! Float,
+            when: localDate
+        )
+        
+        return currency
+    }
+}
